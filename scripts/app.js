cb1076d309023acdba85ac97a865a7474b98a708
juanigna <jibocchi14@gmail.com>
2022-09-12 22:12:40 -0300

    Seccion vueltas añadida

--- scripts/app.js
+++ scripts/app.js
@@ -1,12 +1,28 @@
 //Variables
+
+//Botones
 const btnAumentar = document.querySelector('#btnAumentar');
 const btnDisminuir = document.querySelector('#btnDisminuir');
 const btnReset = document.querySelector('#btnReset');
 const btnCrono = document.querySelector('#btnCronometro');
+const btnVuelta = document.querySelector('#btnVuelta');
+
+//Variable del contador
 const contador = document.querySelector('#showContador');
+
+//Deshabilito el boton de marcar vueltas hasta que inicie el cronometro
+btnVuelta.disabled = true;
+
+//Contenedor de las vueltas y la lista
+const vueltasContainer = document.querySelector('#vueltas');
+const ul = document.createElement('ul');
+
+//Variables varias
+let numeroVuelta = 1;
 let running = false;
 let newContador = 0;
 let id;
+
 //EventListener
 
 EventListeners();
@@ -17,8 +33,13 @@ function EventListeners() {
     document.addEventListener('click', (e) => {
         contadorLogica(e);
     });
+
+    btnVuelta.addEventListener('click', () => {
+        vueltasCrono();
+    });
 }
 
+//Funcion principal encargada de la logica
 function contadorLogica(e) {
     //En el caso que toque el boton aumentar
     if (e.target.id == 'btnAumentar') {
@@ -35,18 +56,20 @@ function contadorLogica(e) {
             id = setInterval(() => {
                 crono();
             }, 1000);
-
             running = true;
+            btnVuelta.disabled = false;
         } else {
             running = false;
             clearInterval(id);
             btnCrono.textContent = 'Iniciar cronometro';
+            btnVuelta.disabled = true;
         }
     }
     contador.textContent = newContador;
     esteticaBtns();
 }
 
+//Funcion encargada de la estetica del contador
 function valoresContador(valor) {
     if (valor >= 1) {
         contador.classList.add('positivo');
@@ -63,27 +86,30 @@ function valoresContador(valor) {
     }
 }
 
+//Funcion encargada de deshabilitar el boton de cronometro
 function esteticaBtns() {
     if (newContador < 0) {
         btnCrono.disabled = true;
     } else {
         btnCrono.disabled = false;
     }
-
-    if (running) {
-        btnAumentar.disabled = true;
-        btnDisminuir.disabled = true;
-        btnReset.disabled = true;
-    } else {
-        btnAumentar.disabled = false;
-        btnDisminuir.disabled = false;
-        btnReset.disabled = false;
-    }
 }
 
+//Funcion encargada de la funcionalidad del cronometro
 const crono = () => {
     newContador++;
     btnCrono.textContent = 'Detener';
     contador.textContent = newContador;
     valoresContador(newContador);
 };
+
+//Funcion encargada de visualizar las vueltas del cronometro
+function vueltasCrono() {
+    const li = document.createElement('li');
+
+    li.innerHTML = `Vuelta n°: ${numeroVuelta} - Valor: ${newContador}`;
+    ul.appendChild(li);
+
+    vueltasContainer.appendChild(ul);
+    numeroVuelta += 1;
+}
